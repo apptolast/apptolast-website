39b41370d48d3f15934cb26e3aae3d9c770fce65
Lex Martez <103444752+lexmartez@users.noreply.github.com>
2025-11-04 12:20:51 +0100

    feat: Add Google Adsense banner to top on website

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -1,33 +1,24 @@
 import { useEffect } from 'react';
 
-const AdBanner = () => {
+const AdBanner = ({ slotId }) => {
   useEffect(() => {
     try {
-      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
-    } catch (e) {
-      console.error("Adsense error", e);
+      // Cada vez que el componente se monta, intenta cargar el anuncio
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.error('Error al cargar el anuncio:', err);
     }
   }, []);
 
   return (
-    <>
-      <div className="md:block fixed top-0 left-0 h-screen w-1/6 z-50">
-        <ins className="adsbygoogle"
-             style={{ display: 'block', width: '100%', height: '100%' }}
-             data-ad-client="ca-pub-6022710211253419"
-             data-ad-slot="9368929024"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-      </div>
-      <div className="md:block fixed top-0 right-0 h-screen w-1/6 z-50">
-        <ins className="adsbygoogle"
-             style={{ display: 'block', width: '100%', height: '100%' }}
-             data-ad-client="ca-pub-6022710211253419"
-             data-ad-slot="1958284813"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-      </div>
-    </>
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-6022710211253419"
+      data-ad-slot={slotId}
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   );
 };
 
