db3233612edeabecf49c854b172a5be7454c078b
hgarcia.alberto <hgarcia.alberto@gmail.com>
2025-11-04 11:05:00 +0000

    Fix ads issues

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -7,7 +7,8 @@ const Clients = () => {
     {
       titleKey: "clients.items.academia_alba.title",
       descriptionKey: "clients.items.academia_alba.description",
-      image: "https://scontent.fbrs5-1.fna.fbcdn.net/v/t1.6435-1/204147201_264208628832707_7581721214844286923_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=106&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=44S2eRaC18oQ7kNvwHb89bd&_nc_oc=AdmKSPUK1IQzBuDCn0Ve_HwJ0zYneuogsWEVELdS2QVVltSV3iYh8RMhrmzS7N1QlanCBOMFd2Y2ihoukWPiPZ9f&_nc_zt=24&_nc_ht=scontent.fbrs5-1.fna&_nc_gid=cNNYka_HXg-vUj8yblBC5g&oh=00_AffCfjjyE4ZCLi3K9MMVaGujyhNrfXRX2OzCurZpnnN0hA&oe=692B61ED"
+      image: "https://scontent.fbrs5-1.fna.fbcdn.net/v/t1.6435-1/204147201_264208628832707_7581721214844286923_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=106&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=44S2eRaC18oQ7kNvwHb899bd&_nc_oc=AdmKSPUK1IQzBuDCn0Ve_HwJ0zYneuogsWEVELdS2QVVltSV3iYh8RMhrmzS7N1QlanCBOMFd2Y2ihoukWPiPZ9f&_nc_zt=24&_nc_ht=scontent.fbrs5-1.fna&_nc_gid=cNNYka_HXg-vUj8yblBC5g&oh=00_AffCfjjyE4ZCLi3K9MMVaGujyhNrfXRX2OzCurZpnnN0hA&oe=692B61ED",
+      link: "https://www.asociacionalba.org.uk/"
     },
   ];
 
@@ -18,9 +19,15 @@ const Clients = () => {
           <h2 className="text-4xl font-bold mb-4">{t('clients.title')}</h2>
           <p className="text-lg text-gray-400">{t('clients.subtitle')}</p>
         </div>
-        <div className="grid md:grid-cols-1 justify-items-center gap-8">
+        <div className="grid md:grid-cols-3 justify-items-center gap-8">
           {clients.map((client, index) => (
-            <div key={index} className="group relative overflow-hidden rounded-xl max-w-sm">
+            <a 
+              key={index} 
+              href={client.link} 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="group relative overflow-hidden rounded-xl max-w-sm"
+            >
               <div className="aspect-w-16 aspect-h-9">
                 <img 
                   src={client.image}
@@ -32,7 +39,7 @@ const Clients = () => {
                 <h3 className="text-xl font-bold mb-2">{t(client.titleKey)}</h3>
                 <p className="text-gray-300">{t(client.descriptionKey)}</p>
               </div>
-            </div>
+            </a>
           ))}
         </div>
       </div>
